41d96402ec1d4af3a4ca8a320e85addcbf9683e2
unknown <tjm3187@gmail.com>
2019-04-07 20:50:54 -0400

    HunterPrey

--- src/level/LevelList.java
+++ src/level/LevelList.java
@@ -4,9 +4,15 @@ import biome.Biome;
 
 public class LevelList {
 	public static LevelConfiguration[] levels = new LevelConfiguration[] {
-			new LevelConfiguration(2,.1,200, Biome.getBiome("Random")),
-			new LevelConfiguration(3,.1,200, Biome.getBiome("Random")),
-			new LevelConfiguration(3,.51,200, Biome.getBiome("Random"))
+			new LevelConfiguration(2,.15,200, Biome.getBiome("Random")),
+			new LevelConfiguration(3,.25,200, Biome.getBiome("Random")),
+			new LevelConfiguration(3,.25,200, Biome.getBiome("Random")),
+			new LevelConfiguration(4,.30,200, Biome.getBiome("Random")),
+			new LevelConfiguration(4,.30,200, Biome.getBiome("Random")),
+			new LevelConfiguration(5,.30,200, Biome.getBiome("Random")),
+			new LevelConfiguration(5,.30,200, Biome.getBiome("Random")),
+			new LevelConfiguration(6,.30,200, Biome.getBiome("Random")),
+			new LevelConfiguration(6,.30,200, Biome.getBiome("Random")),
 	};
 	
 	public static LevelConfiguration getLevelConfiguration(int number) {
