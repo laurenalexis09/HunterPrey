cc4128d6c07554af7bd192693a74708a4964a012
Cugliotta <cugliottal@etown.edu>
2019-04-23 19:12:04 -0400

    Customized the powerups to look like beakers

--- src/render/PowerupRenderer.java
+++ src/render/PowerupRenderer.java
@@ -1,5 +1,7 @@
 package render;
 
+import java.awt.Color;
+
 import powerups.InvincibilityPowerup;
 import powerups.Powerup;
 import utilities.StdDraw;
@@ -7,7 +9,16 @@ import utilities.StdDraw;
 public class PowerupRenderer {
 
 	public void render(Powerup power) {
+		StdDraw.setPenColor(new Color(108, 47, 0));
+		StdDraw.filledSquare(power.x, power.y+0.05, 0.007);
+		StdDraw.setPenColor(StdDraw.WHITE);
+		StdDraw.filledRectangle(power.x, power.y+0.01, 0.005, 0.03);
+		StdDraw.filledRectangle(power.x, power.y+0.04, 0.01, 0.005);
 		StdDraw.setPenColor(power.color);
+		StdDraw.filledRectangle(power.x, power.y+0.005, 0.005, 0.02);
 		StdDraw.filledCircle(power.x, power.y, 0.02);
+		StdDraw.setPenColor();
+		StdDraw.line(power.x-0.02, power.y, power.x, power.y);
+		StdDraw.line(power.x-0.019, power.y-0.008, power.x, power.y-0.008);
 	}
 }
